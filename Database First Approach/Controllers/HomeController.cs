44814bcd5960980e2d563dce7f0bb75318d28382
Mustafa-Hasnain <mustafahasnain19@gmail.com>
2022-03-17 02:43:18 +0500

    DB First Approach

---
+++ Database First Approach/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Database_First_Approach.Models;
+
+namespace Database_First_Approach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        db_ASPEntities db = new db_ASPEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(signup s)
+        {
+            db.signups.Add(s);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
